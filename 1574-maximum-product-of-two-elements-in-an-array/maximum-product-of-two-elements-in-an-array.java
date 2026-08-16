4e8339eb2d67f19d2d0840dba6a3a3525f589191
yaduvendukunal-eng <yaduvendukunal@gmail.com>
2026-08-16 21:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (58.23%) - LeetSync

---
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int largest = 0;
+        int secondLargest = 0;
+
+        for (int num : nums) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest) {
+                secondLargest = num;
+            }
+        }
+
+        return (largest - 1) * (secondLargest - 1);
+    }
+}
