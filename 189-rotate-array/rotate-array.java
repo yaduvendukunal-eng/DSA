ccb2e980a94d9b0ddb9770e4416536fc8d8c80fd
yaduvendukunal-eng <yaduvendukunal@gmail.com>
2026-08-26 11:24:37 +0530

    Time: 7 ms (5.06%) | Memory: 269 MB (6.45%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,29 +1,16 @@
 class Solution {
+        public static void rev (int a,int n ,int [] nums ){
+            for(int i=a;i<a+(n-a)/2;i++){
+                int temp = nums[i];
+                nums[i]=nums[n-1-(i-a)];
+                nums[n-1-(i-a)]=temp;
+            }
+        }
     public void rotate(int[] nums, int k) {
-        int n= nums.length; 
+        k=k%nums.length;
+        rev(0,nums.length,nums);
+        rev(0,k,nums);
+        rev(k,nums.length,nums);
 
-        for(int i=0;i<n/2;i++){
-            int temp=nums[i];
-            nums[i]=nums[n-1-i];
-            nums[n-1-i] = temp;
-        
-         }
-
-        k= (k % n) ;
-
-        for(int i=0;i<((k)/2);i++){
-
-            int temp = nums[i];
-
-            nums[i]=nums[k-1-i];
-
-            nums[k-1-i]=temp;
-        }
-        for(int i=k;i<k+(n-k)/2;i++){
-
-            int tmp = nums[i];
-            nums[i]=nums[n-1-(i-k)];
-            nums[n-1-(i-k)]=tmp;
-        }
     }
 }
